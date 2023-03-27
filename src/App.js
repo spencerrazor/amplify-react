c1700a826c624244541510abc9c6182aefba2243
spencerrazor <spencerrazor@gmail.com>
2023-03-27 02:08:42 -0700

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { Storage, API } from "aws-amplify";
 import {
   Button,
   Flex,
@@ -10,24 +10,13 @@ import {
   TextField,
   View,
   withAuthenticator,
+  Image
 } from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-
-import { API, Storage } from 'aws-amplify';
-import {
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from '@aws-amplify/ui-react';
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
